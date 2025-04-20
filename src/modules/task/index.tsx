36f7de18e263485b7042a5ac7476e72b4da10d01
wahibadiyatma17 <wahib.adiyatma17@gmail.com>
2025-04-20 17:55:44 +0700

    feat(link): add suspense

--- src/modules/task/index.tsx
+++ src/modules/task/index.tsx
@@ -2,11 +2,12 @@
 
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react';
 
 export default function RedirectTask() {
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
   const taskId = searchParams.get('id');
-  const [url, setUrl] = useState('#')
+  const [url, setUrl] = useState('#');
 
   useEffect(() => {
     if (!taskId) return;
@@ -16,13 +17,12 @@ export default function RedirectTask() {
     const isAndroid = /android/i.test(userAgent);
     const isIOS = /iPad|iPhone|iPod/.test(userAgent) && !('MSStream' in window);
 
-
     const schemeUrl = `mysafety://task/${taskId}`;
     const iosStoreUrl = 'https://apps.apple.com/app'; // TODO: replace with your iOS App Store link
     const androidStoreUrl = 'https://play.google.com/store/apps'; // TODO: Replace with your Play Store link
 
     const fallbackUrl = isIOS ? iosStoreUrl : isAndroid ? androidStoreUrl : 'https://mysafety.id';
-    setUrl(fallbackUrl)
+    setUrl(fallbackUrl);
     // Attempt to open app
     window.location.href = schemeUrl;
 
@@ -35,11 +35,13 @@ export default function RedirectTask() {
   }, [taskId]);
 
   return (
-    <main style={{ padding: 20 }}>
-      <h1>Opening MySafety App...</h1>
-      <p>
-        If nothing happens, <a href={url}>tap here</a>.
-      </p>
-    </main>
+    <Suspense>
+      <main style={{ padding: 20 }}>
+        <h1>Opening MySafety App...</h1>
+        <p>
+          If nothing happens, <a href={url}>tap here</a>.
+        </p>
+      </main>
+    </Suspense>
   );
 }

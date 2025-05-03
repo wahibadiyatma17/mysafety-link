b647c404adf534f18243f3ff9058da9b29c1f5a4
wahibadiyatma17 <wahib.adiyatma17@gmail.com>
2025-05-04 00:55:26 +0700

    chore: change reset password route

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,7 +1,7 @@
 import RedirectResetPassword from '@/modules/reset-password';
 import { Suspense } from 'react';
 
-export default function RedirectTaskPage() {
+export default function RedirectResetPasswordPage() {
   return (
     <Suspense>
       <RedirectResetPassword />
